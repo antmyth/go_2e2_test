64c59da2c654691c0f877cdbee75d2e2d85fc481
Antonio Nascimento <antonio.p.nascimento@gmail.com>
2015-07-30 10:28:01 +0100

    Updating script

--- src/main/scripts/script.sh
+++ src/main/scripts/script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-
+echo "***************************"
+echo "    Starting to deploy     "
 echo "app1 v"${go-main1.version}
 echo "app2 v"${go-main2.version}
+echo "***************************"
